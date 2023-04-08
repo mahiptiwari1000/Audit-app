9d343c00b101d3b78b69922e45e2612d174db09f
mahiptiwari1000 <mahiptiwari1000@gmail.com>
2023-04-09 00:59:38 +0530

    Updated form

--- src/pages/dashboard/NewEntry.js
+++ src/pages/dashboard/NewEntry.js
@@ -3,51 +3,53 @@ import styles from "./styles/newEntryStyles.module.css";
 import Button from "../../../src/components/Button";
 import { BsArrowLeftCircleFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import { gradingData } from "../../utils/constants";
+import { scoreData } from "../../utils/constants";
 
 const formFields = [
   {
     question: "FBO Name",
     inputType: "text",
     id: "restaurantName",
-    placeholder: 'Enter the restaurant name'
+    placeholder: "Enter the restaurant name",
   },
   {
-    question:"Audit Name",
+    question: "Audit Name",
     inputType: "text",
-    id:"auditorName",
-    placeholder: "Enter the auditor name"
+    id: "auditorName",
+    placeholder: "Enter the auditor name",
   },
   {
-    question:"FBO's representative",
-    inputType:"text",
-    id:"fboRepresentativeName",
-    placeholder:"Representative name"
+    question: "FBO's representative",
+    inputType: "text",
+    id: "fboRepresentativeName",
+    placeholder: "Representative name",
   },
   {
     question: "Address",
     inputType: "text",
     id: "address",
-    placeholder: 'Enter the address'
+    placeholder: "Enter the address",
   },
   {
     question: "FBO License No",
     inputType: "number",
     id: "licenseNo",
-    placeholder: 'Enter License No'
+    placeholder: "Enter License No",
   },
   {
     question: "Date",
     inputType: "number",
     id: "dateOfAudit",
-    placeholder: 'Date of Audit'
+    placeholder: "Date of Audit",
   },
 ];
 
 const NewEntry = () => {
   const [formData, setFormData] = useState({
     restaurantName: "",
-    auditorName:"",
-    fboRepresentativeName:"",
+    auditorName: "",
+    fboRepresentativeName: "",
     address: "",
     licenseNo: null,
     dateOfAudit: null,
@@ -69,31 +71,69 @@ const NewEntry = () => {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <BsArrowLeftCircleFill onClick={onClickBackButton} />
-      <div className={styles.title}>Create A New Entry</div>
-      <div className={styles.formBody}>
-        <div className={styles.formQuestions}>
-          {formFields.map((field) => {
-            return <div className={styles.formQuestion}>{field.question}</div>;
-          })}
-        </div>
-        <div className={styles.formInputs}>
-          {formFields.map((field) => {
-            return (
-              <div className={styles.formInput}>
-                <input
-                  type={field.inputType}
-                  value={formData[`${field.id}`]}
-                  placeholder={field.placeholder}
-                  onChange={(e) => handleFormInput(e, field.id)}
-                />
-              </div>
-            );
-          })}
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>Create A New Entry</div>
+        <div className={styles.formBody}>
+          <div className={styles.formQuestions}>
+            {formFields.map((field) => {
+              return (
+                <div className={styles.formQuestion}>{field.question}</div>
+              );
+            })}
+          </div>
+          <div className={styles.formInputs}>
+            {formFields.map((field) => {
+              return (
+                <div className={styles.formInput}>
+                  <input
+                    type={field.inputType}
+                    value={formData[`${field.id}`]}
+                    placeholder={field.placeholder}
+                    onChange={(e) => handleFormInput(e, field.id)}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="table-style">
+        <caption>Grading Data</caption>
+        <table>
+          {gradingData.map((d) => {
+            return (
+              <tr>
+                <td>{d.grade}</td>
+                <td>{d.range}</td>
+                <td>{d.category}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+
+      <div className="table-style">
+        <table>
+          <tr>
+            <th>S.No</th>
+            <th>Audit Areas</th>
+            <th>Max Score</th>
+            <th>Obtained Score</th>
+          </tr>
+          {scoreData.map((d) => {
+            return (
+              <tr>
+                <td>{d.sno}</td>
+                <td>{d.auditAreas}</td>
+                <td>{d.maxScore}</td>
+                <td>{d.obtainedScore}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </>
   );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,4 +17,48 @@ export const NavbarItems = [
   },
 ];
 
-export const NewEntryFormItems = [];
+export const gradingData = [
+  { grade: "A+", range: "95-106", category: "Compliance - Exemplar" },
+  { grade: "A", range: "83-94", category: "Compliance - Satisfactory" },
+  { grade: "B", range: "53-82", category: "Needs Improvement" },
+  { grade: "No grade", range: "<53", category: "Non Compliance" },
+];
+
+export const scoreData = [
+  {
+    sno: "",
+    auditAreas: "Regulatory Compliance",
+    maxScore: 2,
+    obtainedScore: 2,
+  },
+  {
+    sno: "I",
+    auditAreas: "Design & facilities",
+    maxScore: 32,
+    obtainedScore: 25,
+  },
+  {
+    sno: "II",
+    auditAreas: "Control of operation",
+    maxScore: 32,
+    obtainedScore: 30,
+  },
+  {
+    sno: "III",
+    auditAreas: "Maintenance & Sanitation",
+    maxScore: 20,
+    obtainedScore: 18,
+  },
+  {
+    sno: "IV",
+    auditAreas: "Personal Hygiene",
+    maxScore: 10,
+    obtainedScore: 10,
+  },
+  {
+    sno: "V",
+    auditAreas: "Training & Records keeping",
+    maxScore: 10,
+    obtainedScore: 10,
+  },
+];
