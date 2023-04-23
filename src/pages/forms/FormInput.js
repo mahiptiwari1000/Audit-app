9ec21e3fcdbbdafe620a5c02ef28b58d00f1dce4
didyouwin07 <vishwasbordia@outlook.com>
2023-04-23 20:14:36 +0530

    Add form input

--- src/pages/forms/FormInput.js
+++ src/pages/forms/FormInput.js
@@ -3,18 +3,50 @@ import styles from "./styles/formInput.module.css";
 import Button from "../../components/Button";
 
 const PromptComponent = (props) => {
-  const { prompt, inputType, setCurrentIdx, maxIdx, idx } = props;
+  const {
+    setFormValues,
+    prompt,
+    inputType,
+    setCurrentIdx,
+    maxIdx,
+    idx,
+    handleSubmit,
+  } = props;
   const [inputValue, setInputValue] = useState("");
   const [comment, setComment] = useState("");
   const handleInput = (e) => {
-    const { value: input } = e;
+    const { value: input } = e.target;
     setInputValue(input);
+    if (idx === 0) setFormValues((prev) => [{ ...prev[0], ans: input }]);
+    else
+      setFormValues((prev) => {
+        const newVal = [...prev, { ans: input }];
+        return newVal;
+      });
   };
 
   const handleCommentInput = (e) => {
     const { value: commentInput } = e.target;
     setComment(commentInput);
+    // if (idx === 0)
+    //   setFormValues((prev) => [{ ...prev[0], comment: commentInput }]);
+    // else
+    //   setFormValues((prev) => {
+    //     const newVal = [];
+    //     prev.forEach((val, index) => {
+    //       if (index === idx) {
+    //         newVal.push({ ...val, comment: commentInput });
+    //       } else {
+    //         newVal.push(val);
+    //       }
+    //       return newVal;
+    //     });
+    //   });
   };
+
+  useEffect(() => {
+    console.log({ idx, maxIdx });
+  }, [idx, maxIdx]);
 
   return (
     <div className={styles.wrapper}>
@@ -36,14 +68,22 @@ const PromptComponent = (props) => {
         />
       </div>
       <div className={styles.nextButton}>
-        <div
-          className={styles.nextButtonText}
-          onClick={() =>
-            setCurrentIdx((prev) => Math.min(prev + 1, maxIdx - 1))
-          }
-        >
-          {idx === maxIdx - 1 ? "Submit" : "Next"}
-        </div>
+        {idx === maxIdx - 1 ? (
+          <div className={styles.nextButtonText} onClick={handleSubmit}>
+            Submit
+          </div>
+        ) : (
+          <div
+            className={styles.nextButtonText}
+            onClick={() => {
+              setCurrentIdx((prev) => Math.min(prev + 1, maxIdx - 1));
+              setInputValue("");
+              setComment("");
+            }}
+          >
+            Next
+          </div>
+        )}
       </div>
     </div>
   );
@@ -52,16 +92,28 @@ const PromptComponent = (props) => {
 export default function FormInput({ data }) {
   //   const [inputValue, setInputValue] = useState("");
   const [idx, setCurrentIdx] = useState(0);
+  const [formValues, setFormValues] = useState([]);
+  const [showTable, setShowTable] = useState(false);
+  const handleSubmit = () => {
+    setShowTable(true);
+    console.log(formValues);
+  };
 
   return (
     <div className={styles.container}>
-      <PromptComponent
-        setCurrentIdx={setCurrentIdx}
-        idx={idx}
-        prompt={data[idx].title}
-        inputType={data[idx].inputType}
-        maxIdx={data.length}
-      />
+      {showTable ? (
+        <div>Submitted</div>
+      ) : (
+        <PromptComponent
+          setCurrentIdx={setCurrentIdx}
+          idx={idx}
+          prompt={data[idx].title}
+          inputType={data[idx].inputType}
+          maxIdx={data.length}
+          setFormValues={setFormValues}
+          handleSubmit={handleSubmit}
+        />
+      )}
     </div>
   );
 }
