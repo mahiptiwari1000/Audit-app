c2f678bfbc5fd69a5fd6351c1bcddb88312df058
mahiptiwari1000 <mahiptiwari1000@gmail.com>
2023-04-23 17:23:30 +0530

    Updated API

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,10 +1,16 @@
 import { gql } from "@apollo/client";
 
-// const ADD_ENTRY = gql`
-// mutation addEntry($formId: ID!,formAnswers) {
-//     addTodo(type: $type)
-//   }
-// `;
+const ADD_ENTRY = gql`
+mutation addEntry($formId: ID!,$formAnswers:[FormAnswerInput]) {
+    addEntry(formId: $formId,formAnswers:$formAnswers)
+  }
+`;
+
+const ADD_FORM = gql`
+mutation addForm($createdBy: ID!,$formTitle:String,$formPrompts:[FormPromptInput]) {
+    addForm(createdBy: $createdBy,formTitle:$formTitle,formPrompts:$formPrompts)
+  }
+`;
 
 const ADD_USER = gql`
   mutation addUser($name: String!, $email: String!, $role: UserRole!) {
@@ -12,8 +18,14 @@ const ADD_USER = gql`
   }
 `;
 
+const UPDATED_ENTRY = gql`
+mutation updateEntry($formId: ID!, $entryId: ID!, $formAnswers:[FormAnswerInput]) {
+  updateEntry(formId: $formId, entryId: $entryId, formAnswers: $formAnswers)
+}
+`;
+
 const mutations = {
-  ADD_USER,
+  ADD_ENTRY,ADD_FORM,ADD_USER,UPDATED_ENTRY
 };
 
 export default mutations;
